07b662e90ced675a727734fc2472ed406fd270b8
andcarnivorous <drewlinguistics01@gmail.com>
2020-12-04 21:31:45 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/andcarnivorous/nld",
     packages=setuptools.find_packages(),
-    install_requires=["nltk==3.4.5", "pandas>=0.25.3"],
+    install_requires=["nltk==3.4.5", "pandas>=0.25.3", "numpy==1.19.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
